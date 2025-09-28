36daaed13519820a7d96a0c82180c66714c13331
Martin Reisinger <MartinReisinger@users.noreply.github.com>
2025-09-28 18:00:54 +0200

    Initial Commit

---
+++ ClosedBikeSensorTests/ClosedBikeSensorTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClosedBikeSensorTests.swift
+//  ClosedBikeSensorTests
+//
+//  Created by Martin Reisinger on 28.09.25.
+//
+
+import Testing
+@testable import ClosedBikeSensor
+
+struct ClosedBikeSensorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
